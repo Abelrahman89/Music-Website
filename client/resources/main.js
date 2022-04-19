d102c5482410beccff66e911a8d0ace6e08c4b18
abdelrahman89 <abdelrahman.freek@gmail.com>
2022-04-19 00:01:37 -0500

    commit6

--- client/resources/main.js
+++ client/resources/main.js
@@ -1,8 +1,9 @@
 let songs = [];
 let playListSongs=[];
+let users=[];
 window.onload = function () {
     getSongs();
-    getPlayList();
+
 
 //login
 
@@ -33,28 +34,8 @@ window.onload = function () {
         } 
         */
     }
-    // getProducts();
-    console.log("hioei");
-    document.getElementById('nav-home').onclick = function (event) {
-        event.preventDefault();
-        getProducts();
-    }
-
-    // add/update product
-   /* document.getElementById('product-btn').onclick = function (event) {
-        console.log("hioi");
-        event.preventDefault();
-        console.log("hii");
-        // console("wesdsdsd");
-        if (!document.getElementById('product-btn').dataset.id) {
-            console.log("hieeei");
-            login();
-            //addProduct();
-        } else {
-            editProduct();
-        }
-    }
-    */
+   
+ 
 }
 
 
@@ -63,10 +44,11 @@ async function login() {
     let password = document.getElementById('password').value;
     let url = 'http://localhost:4040/users?username=' + username + '&&password=' + password;
     console.log('url' + url);
-    let products = [];
-    products = await fetch(url).then(response => response.json());
-    if (products.username == username) {
+    
+    users = await fetch(url).then(response => response.json());
+    if (users.username == username) {
         console.log("success");
+        getPlayListByUserId();
     }
     else
         alert("sorry username or password incorrect");
@@ -100,12 +82,12 @@ async function getSongs() {
 
 }
 
-async function getPlayList() {
+async function getPlayListByUserId() {
     for (let i = 0; i < songs.length; i++) {
         deleteSongFunction(i);
     }
   
-    let url = 'http://localhost:4040/playLists' ;
+    let url = 'http://localhost:4040/playLists/'+users.id;
     console.log('url' + url);
     
     playListSongs = await fetch(url).then(response => response.json());
@@ -113,6 +95,35 @@ async function getPlayList() {
 
 
 }
+
+async function addSonginplayList(songId,songTitle) {
+
+ if(playListSongs.songs.length>0)
+ {
+    console.log("hi");
+    playListSongs.songs.id=songId;
+    playListSongs.songs.title=songTitle;
+ }
+ console.log(playListSongs.songs);
+ console.log(playListSongs.userId);
+ console.log(playListSongs.playListId);
+
+ for (let i = 0; i < playListSongs.songs.length; i++) {
+    console.log("n"+playListSongs.songs[i].songId);
+ }
+        let result = await fetch('http://localhost:4040/playLists/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId: playListSongs.userId,
+                playListId: playListSongs.playListId,
+                songs:  playListSongs.songs
+            })
+        }).then(res => res.json());
+   
+    }
 
 async function searchBySongTitle() {
     console.log('sdsdsdsdsd');
@@ -140,81 +151,13 @@ async function searchBySongTitle() {
 
 
 }
-async function getProducts() {
-    
 
-    products.forEach(prod => renderProduct(prod));
-}
 
-function renderProduct(prod) {
-    console.log("hiii");
-    const div = document.createElement('div');
-    div.classList = 'col-lg-4';
-    div.id = prod.id;
-    div.innerHTML = `<svg class="bd-placeholder-img rounded-circle" width="140" height="140"  aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-   
-    <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777"
-        dy=".3em">140x140</text>
-    </svg>`;
-    /*  div.innerHTML = `<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-      <title>Placeholder</title>
-      <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777"
-          dy=".3em">140x140</text>
-      </svg>`;*/
 
-    const h3 = document.createElement('h3');
-    const h2 = document.createElement('h2');
-    h2.textContent = prod.title;
-    h3.textContent = "test";
-    const price = document.createElement('p');
-    price.textContent = prod.price;
-
-    const description = document.createElement('p');
-    description.textContent = prod.description;
-
-    div.appendChild(h2);
-    div.appendChild(h3);
-    div.appendChild(price);
-    div.appendChild(description);
-
-    const actions = document.createElement('p');
-    const updateBtn = document.createElement('a');
-    updateBtn.classList = 'btn btn-secondary';
-    updateBtn.textContent = 'UPDATE';
-    updateBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        document.getElementById('product-heading').textContent = 'Edit Product';
-        document.getElementById('title').value = prod.title;
-        document.getElementById('price').value = prod.price;
-        document.getElementById('description').value = prod.description;
-        document.getElementById('product-btn').dataset.id = prod.id;
-    });
-
-    const deleteBtn = document.createElement('a');
-    deleteBtn.classList = 'btn btn-secondary';
-    deleteBtn.textContent = 'DELETE';
-    deleteBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        fetch('http://localhost:3000/products/' + prod.id, {
-            method: 'DELETE',
-        }).then(response => {
-            alert('Delete Successfully!');
-            div.remove();
-        });
-    });
-
-    actions.appendChild(updateBtn);
-    actions.appendChild(deleteBtn);
-
-    div.appendChild(actions);
-
-    document.getElementById('products').appendChild(div);
-}
 
 
 async function addProduct() {
-    console.log("hi");
+//    console.log("hi");
 
     let result = await fetch('http://localhost:3000/products/', {
         method: 'POST',
@@ -232,41 +175,14 @@ async function addProduct() {
     renderProduct(result);
 }
 
-function editProduct() {
-    console.log("hi");
-    const prodId = document.getElementById('product-btn').dataset.id;
-    const title = document.getElementById('title').value;
-    const price = document.getElementById('price').value;
-    const description = document.getElementById('description').value;
-    fetch('http://localhost:3000/products/' + prodId, {
-        method: 'PUT',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            title: title,
-            price: price,
-            description: description
-        })
-    }).then(response => response.json())
-        .then(jsonObj => {
-            const productDiv = document.getElementById(prodId);
-            productDiv.querySelector('h2').textContent = title;
-            const paragraphArr = productDiv.querySelectorAll('p');
-            paragraphArr[0].textContent = price;
-            paragraphArr[1].textContent = description;
 
-            document.getElementById('product-heading').textContent = 'Add a new Product';
-            document.getElementById('product-btn').dataset.id = '';
-            document.getElementById('product-form').reset();
-        });
-}
 
 
 function addSongFunction(i) {
     // console.log('e.target.classList'+event.target);
 
-    console.log(i)
+//    console.log(i)
+addSonginplayList(songs[i].id,songs[i].title);
         const tbodyE2 =  document.getElementById("tb2");
         tbodyE2.innerHTML  += `
         <tr id=${i}>
@@ -282,14 +198,14 @@ function renderPlayList() {
 
    
         const tbodyE2 =  document.getElementById("tb2");
-        let cnt =playListSongs;
-        console.log(cnt[0].songs);
+        //let cnt =playListSongs;
+       // console.log(playListSongs.songs);
 
-        for (let i = 0; i < playListSongs[0].songs.length; i++) {
+        for (let i = 0; i < playListSongs.songs.length; i++) {
             tbodyE2.innerHTML += `
-                <tr>
-                    <td>${playListSongs[0].songs[i].id}</td>
-                    <td>${playListSongs[0].songs[i].title}</td>                   
+                <tr id=${i}>
+                    <td>${playListSongs.songs[i].id}</td>
+                    <td>${playListSongs.songs[i].title}</td>                   
                     <td><button class="deleteBtn"  onclick="deleteSongFunction(${i})" id="remove-song"${i}>Delete</button></td>
                  
                 </tr>
@@ -302,13 +218,13 @@ function renderPlayList() {
 
 function deleteSongFunction(i) {
    
-
+    console.log(i);
     var index, table = document.getElementById('playlist');
    // console.log(table.rows[0]);
     for(var i = 1; i < table.rows.length; i++)
     {
-        console.log(table.rows[i].cells[3]);
-        table.rows[i].cells[3].onclick = function()
+        console.log(table.rows[i].cells[2]);
+        table.rows[i].cells[2].onclick = function()
         {
             var c = confirm("do you want to delete this row");
             if(c === true)
